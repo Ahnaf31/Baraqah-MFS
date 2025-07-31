3bcf606815f2ddafbc684689ed8f44fb95b96f31
Ahnaf Shikder Arnob <ahnafarnob31@gmail.com>
2025-07-31 11:33:42 +0600

    Added add money form and event handler inprogress

--- js/home.js
+++ js/home.js
@@ -1,1 +1,12 @@
-console.log("home script loaded");
+document
+  .getElementById("add-money-btn")
+  .addEventListener("click", function (event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    const addMoneyInput = document.getElementById("input-addmoney").value;
+    const addMoneyAmount = parseFloat(addMoneyInput);
+    console.log(addMoneyInput);
+
+    const inputPinNumber = document.getElementById("input-pin-number").value;
+    console.log(inputPinNumber);
+  });
